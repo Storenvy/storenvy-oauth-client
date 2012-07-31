184a2d6f3b6896409c276f06d7a046826898915f
Corey Reece <corey@storenvy.com>
2012-07-31 13:20:32 -0700

    Overcome failure

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,14 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_or_initialize_for_storenvy_oauth(oauth_data)
     @user.update_storenvy_credentials(oauth_data)
     @user.save!
-
     sign_in_and_redirect @user
+  end
+
+  def failure
+  	Rails.logger.info request.env["omniauth.auth"].inspect
+    Rails.logger.info request.env['omniauth.error'].inspect
+    Rails.logger.info request.env['omniauth.error.type'].inspect
+    Rails.logger.info request.env['omniauth.error.strategy'].inspect
+    super
   end
 end
