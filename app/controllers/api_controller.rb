fcec62c96915a1b8c1cedbfc06af62d01e4aaa9c
Ben Koonse <bkoonse@gmail.com>
2012-08-01 23:28:34 -0700

    hacking a fix to allow for arbitrary query string params

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,10 +8,19 @@ class ApiController < ApplicationController
         session[:user_id]      = nil
         session[:access_token] = nil
       end
-      @json = storenvy_access_token.get("/v1/#{params[:api]}").parsed
+      @json = storenvy_access_token.get("/v1/#{params[:api]}.json#{aggregate_params}").parsed
     rescue => e
       @json = { message: "An Oauth excecption occurred. You may have requested something you don't have access to." }
     end
-    respond_with @json      
+    respond_with @json
+  end
+
+  # HACK
+  def aggregate_params
+    param_str = '?'
+    request.query_parameters.each do |param|
+      param_str += "#{param.first}=#{param.second.gsub('.json','')}&"
+    end
+    param_str
   end
 end
